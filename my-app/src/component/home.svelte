377fc1479977abc183a5b614d88903dea6f9f3fe
Amjad-722 <aliamjad722ee@gmail.com>
2025-07-22 13:58:43 +0500

    intilized my repo of portfolio

---
+++ my-app/src/component/home.svelte
@@ -0,0 +1,17 @@
+<script>
+
+
+
+
+</script>
+
+
+<div>
+
+  <nav class="flex flex-col items-center bg-gray-600 py-4 ">
+    <ul class="flex flex-row justify-between gap-3 text-white font-semibold text-[14px]"><li>home</li>
+    <li> services</li>
+    <li>contact</li>
+    <li>footer</li></ul>
+  </nav>
+</div>
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Home from "../component/home.svelte";
+
+
+</script>
+
+
+
+<main class="h-dvh w-full bg-gray-400">
+   <Home/>
+</main>
